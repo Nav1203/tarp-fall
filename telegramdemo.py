0938d044c31b4db058e3eddc7dca2fe356ac322e
Nav1203 <99076767+Nav1203@users.noreply.github.com>
2023-04-17 19:07:25 +0530

    Update telegramdemo.py

--- telegramdemo.py
+++ telegramdemo.py
@@ -1,7 +1,7 @@
 import requests
 def tel_bot_send_msg(body):
     chat_id='fall_det'
-    str='https://api.telegram.org/bot5883212820:AAHqrVFj7LpaKVGNJ89MOZQ_p7WKBndne6A/sendMessage?chat_id=@'+chat_id+'&text='+body
+    str='''Your API link for telegram bot'''+chat_id+'&text='+body
     x = requests.get(str)
 
 if __name__=="__main__":
