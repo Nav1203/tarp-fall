dd8e56490b54444c47d881b27deee88a1a6724b1
Nav1203 <99076767+Nav1203@users.noreply.github.com>
2023-04-17 19:10:22 +0530

    Update sendnotification.py

--- sendnotification.py
+++ sendnotification.py
@@ -1,17 +1,10 @@
 import whatsappdemo
 import telegramdemo
 import geocoder
-def send_img():
-    phone_numbers=['919188590190','917907779230','918618841325','917034209186']
-    for i in phone_numbers:
-        whatsappdemo.send_whtsp_img('temp.jpg',i)
 def send(img_url):
-    phone_numbers=['919188590190','917907779230','918618841325','917034209186']
+    number='''Phone Number the message is to be sent to'''
     g=geocoder.ip('me')
     msg_str="Fall Detected at Location "+"http://maps.google.com/maps?q="+str(g.latlng[0])+","+str(g.latlng[1])+" Image: "+img_url
     print(msg_str)
-    for i in phone_numbers:
-        whatsappdemo.send_whtsp_msg(msg_str,i)
+    whatsappdemo.send_whtsp_msg(msg_str,number)
     telegramdemo.tel_bot_send_msg(msg_str)
-if __name__=='__main__':
-    send()
